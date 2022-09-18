0790cdf2a988f531a167902865df6d641015b141
ABuffaloHerd <62997882+ABuffaloHerd@users.noreply.github.com>
2022-09-19 11:59:06 +1200

    Added working router. slkjdfhajsdkf

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,37 +1,35 @@
 import React, {Component} from 'react';
-import {Router, Route, Routes} from 'react-router'
-import Navlink from 'react-router-dom'
+import
+{
+    createBrowserRouter,
+    RouterProvider,
+    Route
+} from "react-router-dom";
 
 import './App.css';
 import Search from "./pages/Search";
-
-// function App() 
-// {
-//     return (
-      
-//     <Router>
-//         <header>TEST</header>
-        
-//         <div>
-//         <Routes>
-//             <Route path = "/Search" element = {<Search/>}/>
-//         </Routes>
-//         </div>
-//     </Router>
-//   );
-// }
 
 class App extends Component
 {
     render()
     {
-    return (
-        <Router>
-            <h1>test</h1>
-            <Navlink to = "/Search"></Navlink>
+        const router = createBrowserRouter
+        (
+            [
+                {
+                    path: "/",
+                    element: <h2>LAKSDHFLKESRJHNF</h2>
+                },
+                {
+                    path: "/Search",
+                    element: <Search/>,
+                }
+            ]
+        )
 
-        </Router>
-    );
+        return(
+            <RouterProvider router={router}/>
+        );
     }
 }
 
