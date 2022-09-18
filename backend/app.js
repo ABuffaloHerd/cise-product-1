a6a684ff705385dfa88f6b7d1e15c62918d8d808
alessandra-h <84106206+alessandra-h@users.noreply.github.com>
2022-09-19 10:40:36 +1200

    Create new branch for deploying to heroku

--- backend/app.js
+++ backend/app.js
@@ -5,6 +5,12 @@ const app = express();
 
 connectDB();
 
+// look for build of react app
+if (process.env.NODE_ENV === 'production') {
+    console.log("true")
+    app.use(express.static('frontend/build'));
+}
+
 app.get("/", (req, res) => res.send("Test"));
 
 const port = process.env.PORT || 8082;
