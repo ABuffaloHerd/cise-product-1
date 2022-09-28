6417343dbe2c1d3fa8cc4bd9a428862495e475d4
ABuffaloHerd <62997882+ABuffaloHerd@users.noreply.github.com>
2022-09-28 17:05:20 +1300

    Add detailed instructions on how to add a page with current architecture

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,7 +1,5 @@
 import React, { Component } from "react";
 
-import { Link } from "react-router-dom";
-import Navbar from "../components/Navbar";
 import Base from "../components/Base";
 
 class Home extends Base
