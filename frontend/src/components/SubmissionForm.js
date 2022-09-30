79c3b39fb4f481dea68590b06efe33fedf8b5474
alessandra-h <84106206+alessandra-h@users.noreply.github.com>
2022-09-30 20:50:21 +1300

    Create basic input form (no backend interaction)

---
+++ frontend/src/components/SubmissionForm.js
@@ -0,0 +1,31 @@
+import React, { useState } from "react";
+import { useForm } from "react-hook-form";
+
+const SubmissionForm = () => {
+  const { register, handleSubmit } = useForm();
+  const [result, setResult] = useState("");
+  const onSubmit = (data) => setResult(JSON.stringify(data));
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+    
+      <input {...register("title")} placeholder="Title" />
+      <p><input {...register("authors")} placeholder="Authors" /></p>
+      <p><input {...register("source")} placeholder="Journal/Conference name" /></p> 
+      <p><input {...register("pubyear")} placeholder="Publication Year" /></p>
+      <p><input {...register("volume")} placeholder="Volume" /></p>
+      <p><input {...register("pages")} placeholder="Pages" /></p>
+      <p><input {...register("doi")} placeholder="DOI" /></p>
+     
+      <select {...register("sepractice")}>
+        <option value="">Select SE practice...</option>
+        <option value="TDD">TDD</option>
+        <option value="Mob Programming">Mob Programming</option>
+      </select>
+
+      <p>{result}</p>
+      <input type="submit" />
+    </form>
+  );
+}
+export default SubmissionForm;
