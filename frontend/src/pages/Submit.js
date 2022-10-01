21a195774d2a99f85d58007477c4f80d85fb5057
ABuffaloHerd <62997882+ABuffaloHerd@users.noreply.github.com>
2022-10-01 23:07:21 +1300

    Added proper fields and basic validation

--- frontend/src/pages/Submit.js
+++ frontend/src/pages/Submit.js
@@ -8,13 +8,49 @@ class Submit extends Base
     {
         super();
 
-        this.key =          React.createRef();
+        // Thank fuck these exist
         this.title =        React.createRef();
         this.description =  React.createRef();
-        this.rating =       React.createRef();
         this.tags =         React.createRef();
-        this.status =       React.createRef();
-        this.value =        React.createRef();
+
+        // dunno what this does but it's required
+        this.onSubmit = this.onSubmit.bind(this);
+    }
+
+    splitTags(string)
+    {
+        var tags = string.split(',');
+
+        return tags;
+    }
+
+    onSubmit(e)
+    {
+        e.preventDefault();
+        
+        // validate that all fields are filled
+        if(!this.title.current.value)
+        {
+            alert("Empty title field.");
+            return;
+        }
+        else if (!this.description.current.value)
+        {
+            alert("Empty description field.");
+            return;
+        } 
+        else if (!this.tags.current.value)
+        {
+            alert("Empty tags field");
+            return;
+        }
+
+        // tags in a nice array for backend to work with~
+        var tags = this.splitTags(this.tags.current.value);
+        console.log(tags)
+        
+        // CODE TO BACKEND GOES HERE I HOPE SOMEONE ELSE IS MONITORING.
+        console.log("submitted");
     }
 
     render()
@@ -24,15 +60,21 @@ class Submit extends Base
                 <h2>Submit an article</h2>
                 {super.getBaseComponents()}
 
-                <form>
+                <form onSubmit={this.onSubmit}>
                     <label>
                         Title:
                         <input type="text" ref={this.title}/>
                     </label>
                     <label>
                         Description:
-                        <input type="text" ref={this.title}/>
+                        <input type="text" ref={this.description}/>
                     </label>
+                    <label>
+                        Tags:
+                        <textarea ref={this.tags}/>
+                    </label>
+
+                    <input type="submit" label="SUBMIT"/>
                 </form>
             </div>
         );  
