03a0365c183563f12856e6ab29a31ce7d0312a32
ABuffaloHerd <62997882+ABuffaloHerd@users.noreply.github.com>
2022-10-01 22:34:44 +1300

    Basic form with two fields

--- frontend/src/pages/Submit.js
+++ frontend/src/pages/Submit.js
@@ -1,9 +1,22 @@
-import React, { Component } from "react";
+import React from "react";
 
 import Base from "../components/Base";
 
 class Submit extends Base
 {
+    constructor()
+    {
+        super();
+
+        this.key =          React.createRef();
+        this.title =        React.createRef();
+        this.description =  React.createRef();
+        this.rating =       React.createRef();
+        this.tags =         React.createRef();
+        this.status =       React.createRef();
+        this.value =        React.createRef();
+    }
+
     render()
     {
         return(
@@ -11,7 +24,16 @@ class Submit extends Base
                 <h2>Submit an article</h2>
                 {super.getBaseComponents()}
 
-
+                <form>
+                    <label>
+                        Title:
+                        <input type="text" ref={this.title}/>
+                    </label>
+                    <label>
+                        Description:
+                        <input type="text" ref={this.title}/>
+                    </label>
+                </form>
             </div>
         );  
     }
