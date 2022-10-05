d641bab7c975b69631f719b3ecccdb3edb7065f8
Coen <coen-yakas@live.com>
2022-10-06 11:37:06 +1300

    redid backend
    - sorry the node_modules cantr seem to be ignored idk why

--- backend/routes/Article-routes.js
+++ backend/routes/Article-routes.js
@@ -1,11 +1,19 @@
 const express = require("express");
 const router = express.Router();
-const articleController = require("../controllers/Article-controllers");
-
-router.get("/", articleController.getAllArticles);
-router.post("/", articleController.addArticle);
-router.get("/:title", articleController.getArticleByTitle);
-router.put("/:id", articleController.updateArticle);
-router.delete("/:id", articleController.deleteArticle);
+const Article = require("../model/Article");
+const ArticleController = require("../controllers/Article-controller");
+/*
+ ** this is all tested and working via Postman and MongoDB Atlas
+ ** if you need to add more routes, just copy and paste the code below
+ ** and make sure you add the logic to the controller not here
+ */
+// Get all articles
+router.get("/", ArticleController.getAllArticles);
+// Add an article
+router.post("/", ArticleController.addArticle);
+// Update an article by id
+router.put("/:title", ArticleController.updateArticle);
+// Delete an article by id
+router.delete("/:title", ArticleController.deleteArticle);
 
 module.exports = router;
---
+++ frontend/src/pages/Submit.js
@@ -0,0 +1,118 @@
+import React from "react";
+import axios from "axios";
+import Base from "../components/Base";
+import "./styles/Form.css";
+
+class Submit extends Base {
+  constructor() {
+    super();
+
+        // Create references for the values holding the form
+        this.title = React.createRef();
+        this.authors = React.createRef();
+        this.source = React.createRef();
+        this.pubyear = React.createRef();
+        this.volume = React.createRef();
+        this.pages = React.createRef();
+        this.doi = React.createRef();
+
+    // dunno what this does but it's required
+    this.onSubmit = this.onSubmit.bind(this);
+  }
+
+  splitTags(string) {
+    var tags = string.split(",");
+
+    return tags;
+  }
+
+  lobster() {
+    var img = require("./images/lob.png"); // need require so webpack accesses resource
+    var lobster = document.getElementById("lobster");
+    var i = document.createElement("img");
+    i.src = img;
+
+    lobster.appendChild(i);
+  }
+
+  onSubmit(e) {
+    e.preventDefault();
+
+    // validate that all fields are filled
+    if (!this.title.current.value) {
+      alert("Empty title field.");
+      return;
+    } else if (!this.description.current.value) {
+      alert("Empty description field.");
+      return;
+    } else if (!this.tags.current.value) {
+      alert("Empty tags field");
+      return;
+    }
+
+    this.lobster();
+
+    // tags in a nice array for backend to work with~
+    var tags = this.splitTags(this.tags.current.value);
+    // console.log(tags)
+
+    // CODE TO BACKEND GOES HERE I HOPE SOMEONE ELSE IS MONITORING.
+    // console.log("submitted");
+
+    const newArticle = {
+      title: this.title.current.value,
+      description: this.description.current.value,
+      tags: tags
+    };
+
+    axios
+      .post("http://localhost:8082/articles/add", newArticle)
+      .then((res) => console.log(res.data));
+
+    this.setState({
+      title: "",
+      description: "",
+      tags: "",
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <h2>Submit an article</h2>
+        {super.getBaseComponents()}
+
+        <div class="formDiv">
+          <form class="textForm" onSubmit={this.onSubmit}>
+            <label>
+              Title:
+              <input type="text" ref={this.title} placeholder="Sample text" />
+            </label>
+            <label>
+              Description:
+              <input
+                type="text"
+                ref={this.description}
+                placeholder="Sample descriptive text"
+              />
+            </label>
+            <label>
+              Tags: (comma separated)
+              <textarea
+                ref={this.tags}
+                rows="3"
+                cols="50"
+                placeholder="asdfmovie1,asdfmovie2"
+              />
+            </label>
+
+            <input type="submit" label="SUBMIT" />
+          </form>
+        </div>
+        <div id="lobster" class="lob"></div>
+      </div>
+    );
+  }
+}
+
+export default Submit;
