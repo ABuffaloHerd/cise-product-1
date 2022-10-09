93b7b77e8003c56d0d29d2463230198301a3eb7e
alessandra-h <84106206+alessandra-h@users.noreply.github.com>
2022-10-09 20:27:49 +1300

    Install cors

--- backend/app.js
+++ backend/app.js
@@ -2,13 +2,26 @@ const express = require("express");
 const connectDB = require("./config/db");
 const router = require("./routes/Article-routes");
 const path = require("path");
+var cors = require('cors');
+
 const app = express();
 
 app.use(express.json);
 app.use("/articles", router);
 app.use(express.static(path.join(__dirname, "public")));
 
+// routes
+const articles = require('./routes/Article-routes');
+
 connectDB();
+
+app.use(cors({ origin: true, credentials: true }));
+
+// Init Middleware
+app.use(express.json({ extended: false }));
+
+// use Routes
+app.use('/Article-routes', articles);
 
 const port = process.env.PORT || 8082;
 
